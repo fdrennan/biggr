b5edbeb178714ad7d5b12369169dc29a2984c5bd
Freddy Drennan <drennanfreddy@gmail.com>
2019-04-27 10:01:50 -0600

    Update boto bug

--- R/initial_set_up.R
+++ R/initial_set_up.R
@@ -32,12 +32,12 @@ configure_aws <- function(aws_access_key_id = NULL,
   aws_secret_access_key <-
     paste("aws configure set aws_secret_access_key", aws_secret_access_key)
 
-  default.region <-
+  default_region <-
     paste("aws configure set default.region", default.region)
 
   subprocess$call(access_key, shell=TRUE)
   subprocess$call(aws_secret_access_key, shell=TRUE)
-  subprocess$call(region, shell=TRUE)
+  subprocess$call(default_region, shell=TRUE)
 
 
 }
