9872009f04bb19034ab973df75b50f014cb45454
Freddy Drennan <drennanfreddy@gmail.com>
2019-05-01 16:36:36 -0600

    Add unit tests.

--- tests/testthat/test-client_cost.R
+++ tests/testthat/test-client_cost.R
@@ -1,5 +1,5 @@
 library(awsR)
 
-test_that("connections return the correct class", {
+test_that("client_cost return the correct class", {
   expect_equal(class(client_cost())[[1]], "botocore.client.CostExplorer")
 })
