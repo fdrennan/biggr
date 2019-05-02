dac4a27fcb5a90f0e45cd54422fbedfdb3092126
Freddy Drennan <drennanfreddy@gmail.com>
2019-05-02 16:52:25 -0600

    Restructure unit tests

--- tests/testthat/test-security_group_delete.R
+++ tests/testthat/test-security_group_delete.R
@@ -1,10 +1,13 @@
 library(awsR)
 library(stringr)
 
+result <- security_group_create()
+
 test_that("resource_s3 return the correct class", {
-  sg_id <- security_group_create()
   expect_equal(
-    str_detect(sg_id, "sg-"),
-    TRUE
+    {
+      security_group_delete(result)
+    },
+    200
   )
 })
