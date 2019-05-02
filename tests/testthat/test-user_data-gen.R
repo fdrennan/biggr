b5d19bdd7b0034f0f00849a7cf4f89b1179fa065
Freddy Drennan <drennanfreddy@gmail.com>
2019-05-02 13:41:22 -0600

    update unit tests

---
+++ tests/testthat/test-user_data-gen.R
@@ -0,0 +1,33 @@
+library(awsR)
+
+test_that("user_data return the correct values", {
+  user_data <-
+    user_data_gen(phone_number = 1111111111,
+                  postgres_password = 'postgrespass')
+
+  expect_equal(
+    stringr::str_detect(user_data,
+                        '1111111111'),
+    TRUE
+  )
+
+  expect_equal(
+    stringr::str_detect(user_data,
+                        'postgrespass'),
+    TRUE
+  )
+})
+
+test_that("user_data return the correct values when no phone supplied", {
+  user_data <-
+    user_data_gen(postgres_password = 'postgrespass')
+
+  expect_equal(
+    stringr::str_detect(user_data,
+                        '5555555555'),
+    TRUE
+  )
+
+
+})
+
