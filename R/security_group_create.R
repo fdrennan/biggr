e21cdf2b4111ffb9000d4fe8fc0e5bb68bd3a59f
Freddy Drennan <drennanfreddy@gmail.com>
2019-05-03 12:31:22 -0600

    update unit tests

--- R/security_group_create.R
+++ R/security_group_create.R
@@ -2,26 +2,27 @@
 #' @importFrom purrr keep
 #' @importFrom dplyr filter
 #' @importFrom dplyr pull
+#' @param  group_name Name of security group
 #' @export security_group_create
-security_group_create <- function() {
+security_group_create <- function(group_name = NA) {
 
   resource <- resource_ec2()
   client <- client_ec2()
 
   security_group_df <- security_group_list()
 
-  if(!any(security_group_df$group_name == "prog_r")) {
+  if(!any(security_group_df$group_name == group_name)) {
     create_security <-
       resource$create_security_group(
-        GroupName='prog_r',
+        GroupName=group_name,
         Description='for automated server'
       )
 
     create_security$authorize_ingress(
-      IpProtocol="tcp",
-      CidrIp="0.0.0.0/0",
-      FromPort=0L,
-      ToPort=65535L
+      IpProtocol = "tcp",
+      CidrIp     = "0.0.0.0/0",
+      FromPort   = 0L,
+      ToPort     = 65535L
     )
 
     security_group_id <-
@@ -29,7 +30,7 @@ security_group_create <- function() {
 
   } else {
     security_group_id = security_group_df %>%
-      filter(group_name == "prog_r") %>%
+      filter(group_name == group_name) %>%
       pull(group_id)
   }
 
