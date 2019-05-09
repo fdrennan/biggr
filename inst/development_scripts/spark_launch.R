a84d5f0722cf4e99834d6b7dfd15bdca42f496e1
Freddy Drennan <drennanfreddy@gmail.com>
2019-05-09 11:20:36 -0600

    Add spark creation functions

--- inst/development_scripts/spark_launch.R
+++ inst/development_scripts/spark_launch.R
@@ -11,73 +11,6 @@ use_virtualenv('biggr')
 #   default.region        = "us-east-2"
 # )
 
-spark_master <- function(
-  InstanceType='t2.medium',
-  KeyName = NA,
-  SecurityGroupId = NA,
-  InstanceStorage = 50
-) {
-
-  running_instances <- ec2_instance_info()
-
-  user_data_ami = paste("#!/bin/bash",
-                        "/home/ubuntu/spark-2.1.0-bin-hadoop2.7/sbin/start-master.sh",
-                        sep = "\n")
-
-  ec2_instance_create(ImageId = 'ami-00c36ebbb7d6aa249',
-                      KeyName = KeyName,
-                      InstanceStorage = InstanceStorage,
-                      SecurityGroupId = SecurityGroupId,
-                      user_data = user_data_ami,
-                      InstanceType = InstanceType)
-
-
-  updated_running_instances <- ec2_instance_info()
-
-  master_data <- filter(
-    updated_running_instances,
-    !updated_running_instances$instance_id %in% running_instances$instance_id
-  )
-
-  master_data
-}
-
-
-spark_slave <- function(
-  InstanceType='t2.medium',
-  KeyName = NA,
-  SecurityGroupId = NA,
-  InstanceStorage = 50,
-  n_instances = 2,
-  master_ip = NULL
-) {
-
-  running_instances <- ec2_instance_info()
-
-  user_data_ami = paste("#!/bin/bash",
-                        paste0("/home/ubuntu/spark-2.1.0-bin-hadoop2.7/sbin/start-slave.sh ", master_ip, ":7077"),
-                        sep = "\n")
-
-  for(instance in 1:n_instances) {
-    ec2_instance_create(ImageId = 'ami-00c36ebbb7d6aa249',
-                        KeyName = KeyName,
-                        InstanceStorage = InstanceStorage,
-                        SecurityGroupId = SecurityGroupId,
-                        user_data = user_data_ami,
-                        InstanceType = InstanceType)
-  }
-
-  updated_running_instances <- ec2_instance_info()
-
-  slave_data <- filter(
-    updated_running_instances,
-    !updated_running_instances$instance_id %in% running_instances$instance_id
-  )
-
-  slave_data
-}
-
-
 master_data <- spark_master(
   KeyName = "Shiny",
   InstanceStorage = 35L,
@@ -90,7 +23,7 @@ slave_data <- spark_slave(
   InstanceStorage = 35L,
   SecurityGroupId = 'sg-0e8841d7a144aa628',
   InstanceType = 't2.medium',
-  n_instances = 30,
+  n_instances = 3,
   master_ip = master_data$public_ip_address
 )
 
