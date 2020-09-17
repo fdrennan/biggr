7b3bda847b1952f0a5a9f57142cf65e4c0b51a73
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-16 18:17:00 -0600

    Remove some files

--- main.R
+++ main.R
@@ -1,220 +1,268 @@
 library(biggr)
 library(dbx)
-# a <- server_info()
-paramiko <- import('paramiko')
+library(ndexssh)
 
+stages <- c('beta', 'master', 'dev')
+master_branch <- 'master'
+if (FALSE) {
+   build_servers <- function() {
+      library(furrr)
+      plan(multiprocess)
+      # Create Security Group
+      security_group_name <- 'production'
+      security_group_description <- 'Ports for Production'
+      key_name <- 'fdren'
+      open_ports <- c(22, 80, 6000, 8000:8020, 8787, 5432, 5439, 3000, 8080)
+      open_ips <- get_ip()
 
-get_server_stage <- function() {
-  con <- suppressMessages(postgres_connector())
-  on.exit(dbDisconnect(con))
-  tbl(con, in_schema('public', 'server_stage')) %>% collect
+      security_group_create(security_group_name = security_group_name,
+                            description = security_group_description)
+
+      security_group_id <-
+         security_group_envoke(sg_name = security_group_name,
+                               ports = open_ports,
+                               ips = open_ips)
+
+      # Create Keyfile
+      keyfile_creation <- tryCatch(expr = {
+         keyfile_create(keyname = key_name)
+      }, error =  function(err) {
+         message(glue('Keyfile {key_name} Already Exists'))
+      })
+
+      servers_objects <-
+         ec2_instance_create(ImageId = 'ami-0010d386b82bc06f0',
+                             InstanceType='t2.large',
+                             min = length(stages),
+                             max = length(stages),
+                             KeyName = key_name,
+                             SecurityGroupId = security_group_id,
+                             InstanceStorage = 30,
+                             DeviceName = "/dev/sda1",
+                             user_data  = readr::read_file('instance.sh'))
+
+      Sys.sleep(60)
+
+      dns_names <- map_chr(servers_objects, function(x) {x$public_dns_name})
+      initial_script_complete <-
+         checking_if_complete(dns_names = dns_names,
+                              username = "ubuntu",
+                              keyfile = "/Users/fdrennan/fdren.pem")
+
+      stage_scripts <-
+         map(
+            stages,
+            function(stage) {
+               command_block <-c(
+                  "#!/bin/bash",
+                  "exec &> /home/ubuntu/dockerlogs.txt",
+                  "ls -lah",
+                  "sudo apt-get update -y",
+                  "git clone https://github.com/fdrennan/docker_pull_postgres.git || echo 'Directory already exists...'",
+                  "docker-compose -f docker_pull_postgres/docker-compose.yml pull",
+                  "docker-compose -f docker_pull_postgres/docker-compose.yml down",
+                  "docker-compose -f docker_pull_postgres/docker-compose.yml up -d",
+                  "docker container ls",
+                  "git clone https://github.com/fdrennan/productor.git",
+                  glue("cd /home/ubuntu/productor && git checkout -b {stage} && git pull origin {stage}"),
+                  glue("cd /home/ubuntu/productor && echo SERVER={stage} >> .env"),
+                  glue("cd /home/ubuntu/productor && echo SERVER={stage} >> .Renviron"),
+                  "cd /home/ubuntu/productor && sudo /usr/bin/Rscript update_env.R",
+                  "cd /home/ubuntu/productor && docker-compose pull",
+                  "cd /home/ubuntu/productor && docker-compose up -d --build productor_postgres",
+                  "cd /home/ubuntu/productor && docker-compose up -d --build productor_initdb",
+                  "cd /home/ubuntu/productor && docker-compose up -d --remove-orphans"
+               )
+            }
+         )
+
+      response <-
+         future_map2(
+            stage_scripts,
+            dns_names,
+            function(script, dns) {
+               script_name <- glue('{dns}script.sh')
+               writeLines(text = script, con = script_name)
+               message(glue('Building {dns}'))
+               send_file(hostname = dns,
+                         username = "ubuntu",
+                         keyfile = "/Users/fdrennan/fdren.pem",
+                         local_path = script_name,
+                         remote_path = glue('/home/ubuntu/{script_name}'))
+               cmd_response <- execute_command_to_server(
+                  command = glue('. /home/ubuntu/{script_name}'),
+                  hostname = dns
+               )
+               cmd_response
+            }
+         )
+   }
+
+   build_servers()
 }
 
-tryCatch(expr = {
-  keyfile_create(keyname = 'fdren')
-}, error =  function(err) {
-  message('Keyfile Already Exists')
-})
 
-security_group_name <- 'production'
 
-security_group_create(security_group_name = security_group_name,
-                      description = 'Ports for Production')
 
-security_group_id <-
-  security_group_envoke(sg_name = security_group_name,
-                        ports = c(22, 80, 8000:8020, 8787, 5432, 5439, 3000, 8080),
-                        get_ip())
-
-server_names <- c('DEV', 'BETA', 'PROD')
-
-response <-
-  ec2_instance_create(ImageId = 'ami-0010d386b82bc06f0',
-                      InstanceType='t2.large',
-                      min = length(server_names),
-                      max = length(server_names),
-                      KeyName = 'fdren',
-                      SecurityGroupId = security_group_id,
-                      InstanceStorage = 30,
-                      DeviceName = "/dev/sda1",
-                      user_data  = readr::read_file('instance.sh'))
-
-specify_server_priority(instance_ids = map_chr(response, ~ .$instance_id),
-                        values = server_names)
-
-sleep_time <- 60
-message(glue('Sleeping for {sleep_time} seconds'))
-Sys.sleep(sleep_time)
-
-# Refresh Database
-servers <- server_info()
-
-map2(
-  response,
-  server_names,
-  function(server, server_name) {
-    server_info()
-    time = 1
-    cumulative_wait = sleep_time
-    while (str_detect(stage_run_command('ls -lah', stage_name = server_name) , 'user_data_running')) {
-      Sys.sleep(time)
-      cumulative_wait = cumulative_wait + time
-      message(glue('You have been waiting for {cumulative_wait} seconds'))
-      cat(stage_run_command(command = 'tail -n 30 logfile.txt'))
-    }
-
-    stage_run_command('ls -lah', stage_name = server_name)
-    stage_run_command('git clone https://github.com/fdrennan/productor.git', stage_name = server_name)
-
-    lower_server <- str_to_lower(server_name)
-    stage_run_command(command = glue('cd /home/ubuntu/productor && git checkout -b {lower_server} && git pull origin {lower_server}'),
-                      stage_name = server_name)
-
-    if (lower_server == 'prod') {
-      stage_transfer_file(local_path = glue('docker.sh'), remote_path = '/home/ubuntu/docker.sh', stage_name = server_name)
-    } else {
-      stage_transfer_file(local_path = glue('docker-{server_name}.sh'), remote_path = '/home/ubuntu/docker.sh', stage_name = server_name)
-    }
-    stage_run_command(glue('echo SERVER={server_name} >> /home/ubuntu/productor/.Renviron'),
-                      stage_name = server_name)
-    stage_run_command('. docker.sh',
-                      stage_name = server_name)
-  }
-)
-
-map(
-  server_names,
-  function(server_name) {
-    message(glue('Monitoring over {server_name}'))
-    lower_server <- str_to_lower(server_name)
-    time = 1
-    cumulative_wait = 0
-    while (!str_detect(stage_run_command('ls -lah', stage_name = server_name) , 'docker_data_complete')) {
-      Sys.sleep(time)
-      cumulative_wait = cumulative_wait + time
-      cat(
-        cat(stage_run_command(command = 'tail -n 30 docker.txt'))
+if (FALSE) {
+   servers <- grab_servers()
+   # map(servers, ~ try(.$terminate()))
+   n <- 0
+   while ( n < 3 ) {
+      ids_to_start <- servers[[1]] %>%
+         filter(state == 'running') %>%
+         select(public_dns_name)
+      #
+      responses <- map(
+         ids_to_start$public_dns_name,
+         function(id) {
+            message(glue('  KEYFILE: ssh -i "fdren.pem" ubuntu@{id}\n'))
+            execute_command_to_server(
+               command = 'tail dockerlogs.txt',
+               hostname = id
+            )
+         }
       )
-    }
-  }
-)
-
-
-sns_send_message(phone_number = Sys.getenv('PHONE'), message = 'All Done')
-# Sys.sleep(30)
-server_info()$instances$stop()
-
-
-library(biggr)
-library(dbx)
-
-start_servers <- function() {
-  instances <- server_info()$instances
-  instances$start()
-  map(
-    iterate(instances$all()),
-    function(x) {
-      x$wait_until_running()
-    }
-  )
-  server_info()
+      n <- n + 1
+      Sys.sleep(1)
+   }
 }
 
-stop_servers <- function() {
-  instances <- server_info()$instances
-  instances$stop()
-  map(
-    iterate(instances$all()),
-    function(x) {
-      x$wait_until_stopped()
-    }
-  )
-  server_info()
+# Check Git Branch of Servers
+if (FALSE) {
+   servers <- grab_servers()
+
+   ids_to_start <- servers[[1]] %>%
+      filter(state == 'running') %>%
+      select(public_dns_name)
+
+   responses <- map(
+      ids_to_start$public_dns_name,
+      function(id) {
+         message(glue('  KEYFILE: ssh -i "fdren.pem" ubuntu@{id}\n'))
+         execute_command_to_server(
+            command = 'cd productor && git branch',
+            hostname = id
+         )
+      }
+   )
 }
 
-stop_servers()
-start_servers()
+# Checkout ALL to a SPECIFC Computer
+if (FALSE) {
 
-lower_server <- str_to_lower(server_names)
-server_names <- c('DEV', 'BETA', 'PROD')
-library(stringr)
-map(server_names, ~ {
-  # server_info()$instances$start()
-  lower_stage <- str_to_lower(.)
-  message(.)
-  if (. == 'PROD') {
-    stage_run_command(command = glue('cd /home/ubuntu/productor && /usr/bin/git pull origin'),
-                      stage_name = .)
-    # undebug(stage_run_command)
-    stage_run_command('cd /home/ubuntu/productor && sudo /usr/bin/Rscript update_env.R', stage_name = .)
-    stage_run_command(glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml pull'), stage_name = .)
-    stage_run_command(glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml pull'), stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up -d --build productor_postgres'),
-      stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml -d --build productor_initdb'),
-      stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml restart'),
-      stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up -d'),
-      stage_name = .)
-  } else {
-    stage_run_command(command = glue('cd /home/ubuntu/productor && /usr/bin/git pull origin {lower_stage}'),
-                      stage_name = .)
-    # undebug(stage_run_command)
-    stage_run_command('cd /home/ubuntu/productor && sudo /usr/bin/Rscript update_env.R', stage_name = .)
-    stage_run_command(glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-{lower_stage}.yaml pull'), stage_name = .)
-    stage_run_command(glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-{lower_stage}.yaml pull'), stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-{lower_stage}.yaml up -d --build productor_postgres'),
-      stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-{lower_stage}.yaml -d --build productor_initdb'),
-      stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-{lower_stage}.yaml restart'),
-      stage_name = .)
-    stage_run_command(
-      glue('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-{lower_stage}.yaml up -d'),
-      stage_name = .)
-  }
- }
-)
-instances$stop()
+   servers <- grab_servers()
+
+   ids_to_start <- servers[[1]] %>%
+      filter(state == 'running') %>%
+      select(public_dns_name)
+
+   responses <- map2(
+      ids_to_start$public_dns_name,
+      stages,
+      function(id, branch) {
+         message(glue('  KEYFILE: ssh -i "fdren.pem" ubuntu@{id}\n'))
+         execute_command_to_server(
+            command = glue('cd productor && git reset --hard'),
+            hostname = id
+         )
+         execute_command_to_server(
+            command = glue('cd productor && git checkout -b {branch} && git pull origin {branch} && git branch'),
+            hostname = id
+         )
+         execute_command_to_server(
+            command = glue('cd productor && git checkout {branch} && git pull origin {branch} && git branch'),
+            hostname = id
+         )
+         execute_command_to_server(
+            command = glue('cd productor && ls -lah'),
+            hostname = id
+         )
+      }
+   )
+}
+
+
+if (TRUE) {
+   servers <- grab_servers()
+
+   ids_to_start <- servers[[1]] %>%
+      filter(state == 'running') %>%
+      select(public_dns_name)
+
+   responses <- map2(
+      ids_to_start$public_dns_name,
+      stages,
+      function(id, branch) {
+         message(glue('  KEYFILE: ssh -i "fdren.pem" ubuntu@{id}\n'))
+         message(paste(rep(branch, 5), collapse = '\n'))
+         execute_command_to_server(
+            command = glue('cd productor && git reset --hard'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && git checkout {branch} && git pull origin {branch} && git branch'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && Rscript update_env.R'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && cat .env'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && cat nginx*'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && cat nginx/nginx.conf'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('docker container ls'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && docker-compose -f docker-compose-{branch}.yaml pull'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && docker-compose -f docker-compose-{branch}.yaml reset'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && docker-compose -f docker-compose-{branch}.yaml down'),
+            hostname = id
+         )
+
+         execute_command_to_server(
+            command = glue('cd ~/productor && docker-compose -f docker-compose-{branch}.yaml up -d'),
+            hostname = id
+         )
+
+      }
+   )
+}
+
 #
-
-
-# cd /home/ubuntu/productor && /usr/bin/git pull origin dev
-# cd /home/ubuntu/productor && /usr/bin/git pull origin dev
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-dev.yaml pull
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-dev.yaml up -d --build productor_postgres
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-dev.yaml up -d --build productor_initdb
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-dev.yaml down
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-dev.yaml up
-
-
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-beta.yaml pull
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-beta.yaml up -d --build productor_postgres
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-beta.yaml up -d --build productor_initdb
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-beta.yaml down
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose-beta.yaml up
-
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml pull
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up -d --build productor_postgres
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up -d --build productor_initdb
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml down
-# cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up
-
-# if (TRUE) {
-#   # server_info()$instances$start()
-#   stage_run_command(command = glue('cd /home/ubuntu/productor && git pull origin beta'),
-#                     stage_name = 'BETA')
-#   stage_run_command('cd /home/ubuntu/productor && sudo /usr/bin/Rscript update_env.R', stage_name = 'BETA')
-#   stage_run_command('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml pull', stage_name = 'BETA')
-#   stage_run_command('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up -d --build productor_postgres', stage_name = 'BETA')
-#   stage_run_command('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml -d --build productor_initdb', stage_name = 'BETA')
-#   stage_run_command('cd /home/ubuntu/productor && /usr/local/bin/docker-compose -f docker-compose.yaml up -d --remove-orphans', stage_name = 'BETA')
-# }
+# library(glue)
+#
+# LOCALHOST_IP <- Sys.getenv('LOCALHOST_IP')
+# LOCALHOST_IP
+#
+#
+#
+#
+#
+# write(nginx_conf, file = file.path(Sys.getenv('PRODUCTOR_HOME'), 'shiny', 'nginx.conf'))
