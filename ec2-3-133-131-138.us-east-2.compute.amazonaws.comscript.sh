23342368648779194e1213800fe89235de7f5ff0
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-17 20:58:11 -0600

    remove ec2

--- ec2-3-133-131-138.us-east-2.compute.amazonaws.comscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exec &> /home/ubuntu/dockerlogs.txt
-ls -lah
-sudo apt-get update -y
-git clone https://github.com/fdrennan/docker_pull_postgres.git || echo 'Directory already exists...'
-docker-compose -f docker_pull_postgres/docker-compose.yml pull
-docker-compose -f docker_pull_postgres/docker-compose.yml down
-docker-compose -f docker_pull_postgres/docker-compose.yml up -d
-touch /home/ubuntu/dockerlogs_complete
